11ed585282d5ca78eeb94e53ced52b9843b9f687
Devansh Gupta <63185388+devansh6012@users.noreply.github.com>
2021-08-15 22:56:42 +0530

    Add files via upload

---
+++ request.js
@@ -0,0 +1,18 @@
+const request = require('request');
+console.log("Before");
+
+// // direct
+// request('http://www.google.com', function (error, response, body) {
+//   console.error('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
+
+request('https://www.google.com/',cb);
+console.log("After");
+
+function cb(error, response, html) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('html:', html); // Print the HTML for the Google homepage.
+}
